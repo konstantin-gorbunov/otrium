dc3a9587fab03f3cbc52fd8060b8c534096b7f47
Kostiantyn Gorbunov <gkboxmail@gmail.com>
2021-12-02 10:48:59 +0100

    - data and UI on GeneralInfoCell

--- Otrium/Views/Cells/GeneralInfoCell.swift
+++ Otrium/Views/Cells/GeneralInfoCell.swift
@@ -41,29 +41,34 @@ class GeneralInfoCell: UICollectionViewCell {
     }
     
     private func setup() {
-        backgroundColor = .red
-        
         resetView()
         
-        imageView.layer.cornerRadius = 44
-        imageView.layer.masksToBounds = true
-        imageView.translatesAutoresizingMaskIntoConstraints = false
-        addSubview(imageView)
-        imageView.widthAnchor.constraint(equalToConstant: 88).isActive = true
-        imageView.heightAnchor.constraint(equalToConstant: 88).isActive = true
-        imageView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16).isActive = true
-        imageView.topAnchor.constraint(equalTo: topAnchor, constant: 16).isActive = true
+        setupImageView()
+        setupNameLabel()
+        setupNicknameLabel()
+        setupEmailLabel()
+        setupFollowersLabel()
+        setupFollowingLabel()
     }
     
     private func updateView(_ viewModel: GeneralInfoViewModel) {
         if let imageUrl = viewModel.imageUrl {
             imageView.load(url: imageUrl)
         }
-        nameLabel.text = viewModel.name
-        nicknameLabel.text = viewModel.nickname
-        emailLabel.text = viewModel.email
-        followersLabel.text = String(viewModel.followers ?? 0)
-        followingLabel.text = String(viewModel.following ?? 0)
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.lineHeightMultiple = 1.07
+        nameLabel.attributedText = NSMutableAttributedString(string: viewModel.name, attributes: [.kern: 2.4,
+                                                                                                  .paragraphStyle: paragraphStyle])
+        paragraphStyle.lineHeightMultiple = 1.28
+        nicknameLabel.attributedText = NSMutableAttributedString(string: viewModel.nickname, attributes: [.kern: 0.4,
+                                                                                                          .paragraphStyle: paragraphStyle])
+        emailLabel.attributedText = NSMutableAttributedString(string: viewModel.email, attributes: [.kern: 0.4,
+                                                                                                    .paragraphStyle: paragraphStyle])
+        paragraphStyle.lineHeightMultiple = 1.19
+        followersLabel.attributedText = NSMutableAttributedString(string: viewModel.followers, attributes: [.kern: 0.4,
+                                                                                                            .paragraphStyle: paragraphStyle])
+        followingLabel.attributedText = NSMutableAttributedString(string: viewModel.following, attributes: [.kern: 0.4,
+                                                                                                            .paragraphStyle: paragraphStyle])
     }
     
     private func resetView() {
@@ -73,5 +78,65 @@ class GeneralInfoCell: UICollectionViewCell {
         emailLabel.text = nil
         followersLabel.text = nil
         followingLabel.text = nil
+    }
+    
+    private func setupImageView() {
+        imageView.layer.cornerRadius = 44
+        imageView.layer.masksToBounds = true
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(imageView)
+        imageView.widthAnchor.constraint(equalToConstant: 88).isActive = true
+        imageView.heightAnchor.constraint(equalToConstant: 88).isActive = true
+        imageView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16).isActive = true
+        imageView.topAnchor.constraint(equalTo: topAnchor, constant: 24).isActive = true
+    }
+    
+    private func setupNameLabel() {
+        nameLabel.font = UIFont.boldSystemFont(ofSize: 32)
+        nameLabel.minimumScaleFactor = 0.5
+        nameLabel.adjustsFontSizeToFitWidth = true
+        nameLabel.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(nameLabel)
+        nameLabel.topAnchor.constraint(equalTo: topAnchor, constant: 30).isActive = true
+        nameLabel.leadingAnchor.constraint(equalTo: imageView.trailingAnchor, constant: 8).isActive = true
+        nameLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16).isActive = true
+    }
+    
+    private func setupNicknameLabel() {
+        nicknameLabel.font = UIFont.systemFont(ofSize: 16)
+        nicknameLabel.minimumScaleFactor = 0.5
+        nicknameLabel.adjustsFontSizeToFitWidth = true
+        nicknameLabel.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(nicknameLabel)
+        nicknameLabel.topAnchor.constraint(equalTo: nameLabel.bottomAnchor).isActive = true
+        nicknameLabel.leadingAnchor.constraint(equalTo: nameLabel.leadingAnchor).isActive = true
+        nicknameLabel.trailingAnchor.constraint(equalTo: nameLabel.trailingAnchor).isActive = true
+    }
+    
+    private func setupEmailLabel() {
+        emailLabel.font = UIFont.boldSystemFont(ofSize: 16)
+        emailLabel.minimumScaleFactor = 0.5
+        emailLabel.adjustsFontSizeToFitWidth = true
+        emailLabel.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(emailLabel)
+        emailLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16).isActive = true
+        emailLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16).isActive = true
+        emailLabel.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 16).isActive = true
+    }
+    
+    private func setupFollowersLabel() {
+        followersLabel.font = UIFont.systemFont(ofSize: 16)
+        followersLabel.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(followersLabel)
+        followersLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16).isActive = true
+        followersLabel.topAnchor.constraint(equalTo: emailLabel.bottomAnchor, constant: 16).isActive = true
+    }
+    
+    private func setupFollowingLabel() {
+        followingLabel.font = UIFont.systemFont(ofSize: 16)
+        followingLabel.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(followingLabel)
+        followingLabel.leadingAnchor.constraint(equalTo: followersLabel.trailingAnchor, constant: 16).isActive = true
+        followingLabel.topAnchor.constraint(equalTo: emailLabel.bottomAnchor, constant: 16).isActive = true
     }
 }
--- Otrium/Views/Models/GeneralInfoViewModel.swift
+++ Otrium/Views/Models/GeneralInfoViewModel.swift
@@ -10,18 +10,26 @@ import Foundation
 struct GeneralInfoViewModel {
 
     let imageUrl: URL?
-    let name: String?
-    let nickname: String?
-    let email: String?
-    let followers: Int?
-    let following: Int?
+    let name: String
+    let nickname: String
+    let email: String
+    private let followersCount: Int
+    private let followingCount: Int
+    
+    var followers: String {
+        return "\(followersCount) followers"
+    }
+    
+    var following: String {
+        return "\(followingCount) following"
+    }
     
     init(user: User?) {
-        name = user?.name
-        nickname = user?.login
-        email = user?.email
-        followers = user?.followers?.totalCount
-        following = user?.following?.totalCount
+        name = user?.name ?? ""
+        nickname = user?.login ?? "" 
+        email = user?.email ?? ""
+        followersCount = user?.followers?.totalCount ?? 0
+        followingCount = user?.following?.totalCount ?? 0
         guard let avatarStrURL = user?.avatarUrl else {
             imageUrl = nil
             return
